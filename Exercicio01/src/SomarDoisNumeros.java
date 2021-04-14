6aaa3d6317156016660e6929ce0146abc7e4d87e
mrdbatista <mrdbatista@mrdbatista>
2021-04-14 10:04:47 -0300

    first commit uploading all files

---
+++ Exercicio01/src/SomarDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]) {
+		// declaracao de variaveis
+		int num1, num2, soma;
+		
+		// leitura
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero");
+		num2 = sc.nextInt();
+		
+		//somar
+		soma = num1 + num2;
+		
+		//mostrar na tela
+		System.out.println("Soma:" + soma);
+	}
+}
